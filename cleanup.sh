208d04a020824e508e726b777abc5331827b3f11
kkanie <116817352+kkanie@users.noreply.github.com>
2024-06-09 18:30:17 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ "$(docker ps -a -q)" ]; then
-  docker container stop -f $(docker ps -a -q)
-  docker container rm -f $(docker ps -a -q)
+  docker container stop $(docker ps -a -q)
+  docker container rm $(docker ps -a -q)
 fi
